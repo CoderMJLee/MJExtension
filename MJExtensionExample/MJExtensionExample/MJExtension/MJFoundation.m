0f11b838213fe76fb3a16cc707606158be368ebc
M了个J <199109106@qq.com>
2014-08-06 09:02:27 +0800

    remove unuse isa from model2keyvalues

--- MJExtensionExample/MJExtensionExample/MJExtension/MJFoundation.m
+++ MJExtensionExample/MJExtensionExample/MJExtension/MJFoundation.m
@@ -15,7 +15,7 @@ static NSArray *_foundationClasses;
 
 + (void)initialize
 {
-    _foundationClasses = @[@"NSNumber",@"NSArray",@"NSMutableArray",@"NSData",@"NSMutableData",@"NSDate",@"NSDictionary",@"NSMutableDictionary",@"NSString",@"NSMutableString"];
+    _foundationClasses = @[@"NSObject", @"NSNumber",@"NSArray",@"NSMutableArray",@"NSData",@"NSMutableData",@"NSDate",@"NSDictionary",@"NSMutableDictionary",@"NSString",@"NSMutableString"];
 }
 
 + (BOOL)isClassFromFoundation:(Class)c
