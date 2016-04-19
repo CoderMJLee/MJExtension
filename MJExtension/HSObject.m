4db49fb5a7ed552efc1b9fe2b5587aae56f824c3
wangze <apple@wxz.local>
2016-04-20 00:25:30 +0800

    添加了hsobject

---
+++ MJExtension/HSObject.m
@@ -0,0 +1,13 @@
+//
+//  HSObject.m
+//  MJExtensionExample
+//
+//  Created by wangze on 16/4/20.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "HSObject.h"
+
+@implementation HSObject
+
+@end
