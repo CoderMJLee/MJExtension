3ae82698296a2b2cd8b9e26734dbf7608af0f39c
Ye WeiZheng <wz1214171150@163.com>
2017-04-19 14:23:33 +0800

    添加了person类

---
+++ Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MJExtensionExample
+//
+//  Created by 叶维政 on 2017/4/19.
+//  Copyright © 2017年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Person : UIViewController
+
+@end
---
+++ Person.m
@@ -0,0 +1,37 @@
+//
+//  Person.m
+//  MJExtensionExample
+//
+//  Created by 叶维政 on 2017/4/19.
+//  Copyright © 2017年 小码哥. All rights reserved.
+//
+
+#import "Person.h"
+
+@interface Person ()
+
+@end
+
+@implementation Person
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
