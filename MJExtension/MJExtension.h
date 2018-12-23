3d340fbef99c0a22362dab673610d8df6a85f4cb
Chendaobin <3257753048@qq.com>
2018-12-23 12:10:13 +0800

    修改了bug，优话了大码

--- MJExtension/MJExtension.h
+++ MJExtension/MJExtension.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014年 小码哥. All rights reserved.
 //  代码地址:https://github.com/CoderMJLee/MJExtension
 //  代码地址:http://code4app.com/ios/%E5%AD%97%E5%85%B8-JSON-%E4%B8%8E%E6%A8%A1%E5%9E%8B%E7%9A%84%E8%BD%AC%E6%8D%A2/5339992a933bf062608b4c57
+// 开往笑的代码
 
 #import "NSObject+MJCoding.h"
 #import "NSObject+MJProperty.h"
