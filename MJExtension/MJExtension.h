f0ffe0e78fb19d9ec71d5e2b2ab1d9dd5dc88988
Chendaobin <3257753048@qq.com>
2018-12-23 12:15:36 +0800

    测试玩的

--- MJExtension/MJExtension.h
+++ MJExtension/MJExtension.h
@@ -7,6 +7,7 @@
 //  代码地址:https://github.com/CoderMJLee/MJExtension
 //  代码地址:http://code4app.com/ios/%E5%AD%97%E5%85%B8-JSON-%E4%B8%8E%E6%A8%A1%E5%9E%8B%E7%9A%84%E8%BD%AC%E6%8D%A2/5339992a933bf062608b4c57
 // 开往笑的代码
+// 用来测试用的，请不要关心，你的学生弄着玩的。
 
 #import "NSObject+MJCoding.h"
 #import "NSObject+MJProperty.h"
