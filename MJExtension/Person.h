794fd7668653665aa8c3b29408c7183a7bdcc6fb
“why” <why@163.com>
2019-05-12 17:51:05 +0800

    添加了Person类

---
+++ MJExtension/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  MJExtension
+//
+//  Created by wind on 2019/5/12.
+//  Copyright © 2019 MJExtension. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MJExtension/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MJExtension
+//
+//  Created by wind on 2019/5/12.
+//  Copyright © 2019 MJExtension. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
