84d52ccf32cf16000e079b22c9647f2566965274
kinarob <kinarobin@outlook.com>
2019-06-29 17:10:53 +0800

    add support for swift package manage

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MJExtension",
+     platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8)
+    ],
+    products: [
+       .library(name: "MJExtension", targets: ["MJExtension"])
+   ],
+   targets: [
+       .target(
+           name: "MJExtension",
+           path: "MJExtension"
+       )
+   ]
+)
