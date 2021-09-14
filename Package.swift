08f6be687d4a5910ef0cdb4cfaffebcd3c187c5d
Frank <472730949@qq.com>
2021-09-14 11:04:01 +0800

    SPM supported

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MJExtension",
+    products: [
+        .library(name: "MJExtension", targets: ["MJExtension"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MJExtension",
+            dependencies: [],
+            path: "MJExtension",
+            exclude: ["Info.plist"],
+            publicHeadersPath: ".",
+            cxxSettings: [
+                .headerSearchPath("."),
+            ]
+        ),
+        // Mixed languages are not supported now. Go MJExtension project to see tests.
+//        .testTarget(
+//            name: "MJExtensionTests",
+//            dependencies: ["MJExtension"],
+//            path: "MJExtensionTests",
+//            exclude: ["Info.plist"]
+//        )
+    ]
+)
